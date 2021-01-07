c963c5e4579ce8a95921379486648c088e37aa10
seancheng <seancheng33@gmail.com>
2021-01-07 15:32:27 +0800

    两个接口，内存的收集和登录失败的收集，初步完成

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2021/1/7
+@Program      : 
+'''
+from rest_framework import serializers
+from .models import *
+
+
+class MemInfoSerrializers(serializers.ModelSerializer):
+    class Meta:
+        model = MemInfoModel
+        fields = ('pk', 'date', 'mem_used', 'men_free', 'mem_total',)
+
+
+class LoginFailedSerrializers(serializers.ModelSerializer):
+    class Meta:
+        model = LoginFailedModel
+        fields = ('pk', 'date', 'fail_name', 'fail_ip',)
---
+++ api/urls.py
@@ -0,0 +1,19 @@
+'''
+@Author       : sean cheng
+@Email        : aya234@163.com
+@CreateTime   : 2021/1/7
+@Program      : 
+'''
+
+from django.conf.urls import include,url
+from rest_framework import routers
+from api import views
+
+route = routers.DefaultRouter()
+
+route.register(r'meminfo',views.MemInfoView)
+route.register(r'loginfailed',views.LoginFailedView)
+
+urlpatterns = [
+    url('api/',include(route.urls)),
+]
