fcaa7cc1fca2c9c714adef55fb3e654e5165e1c2
seancheng <seancheng33@gmail.com>
2021-01-12 16:54:38 +0800

    添加echarts的图标

--- datashow/views.py
+++ datashow/views.py
@@ -6,7 +6,8 @@ from api.models import *
 
 # Create your views here.
 def index(request):
+    # 因为这里我是要做单机的，所以这个我只取最后的数据
     result = MechineInfo.objects.last()
-    print(result)
+
     context = {"result":result}
     return render(request, "index.html", context)
