108d981a80a3c7c1541582f40136190f79db0083
seancheng <seancheng33@gmail.com>
2021-01-13 17:55:20 +0800

    显示内存24小时内信息的图标功能基本完成

--- datashow/views.py
+++ datashow/views.py
@@ -1,4 +1,6 @@
 import json
+import time
+from datetime import datetime,timedelta
 
 from django.shortcuts import render
 from api.models import *
@@ -9,5 +11,12 @@ def index(request):
     # 因为这里我是要做单机的，所以这个我只取最后的数据
     result = MechineInfo.objects.last()
 
-    context = {"result":result}
+    # 获取当前的时间，用于获取数据时的时间范围用，设定的开始时间和结束时间为当前时间的24小时内
+    now_time = datetime.now()
+
+    start_date = now_time - timedelta(hours=23, minutes=59, seconds=59)
+    end_date = now_time
+    memory_data = MemInfo.objects.filter(date__range=(start_date, end_date))
+    # print(memory_data)
+    context = {"result": result, "memory_data": memory_data,}
     return render(request, "index.html", context)
