d6b8b4cffdac57f85124821fe91a94200f1d622d
seancheng <seancheng33@gmail.com>
2021-04-18 20:17:32 +0800

    修改首页的显示时间范围为30分钟的内容

--- datashow/views.py
+++ datashow/views.py
@@ -1,6 +1,6 @@
 import json
 import time
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 from django.shortcuts import render
 from api.models import *
@@ -14,7 +14,8 @@ def index(request):
     # 获取当前的时间，用于获取数据时的时间范围用，设定的开始时间和结束时间为当前时间的24小时内
     now_time = datetime.now()
 
-    start_date = now_time - timedelta(hours=23, minutes=59, seconds=59)
+    # start_date = now_time - timedelta(hours=23, minutes=59, seconds=59)
+    start_date = now_time - timedelta(minutes=29, seconds=59)
     end_date = now_time
     # 这里获取的是内存的信息，使用的是时间范围。
     memory_data = MemInfo.objects.filter(date__range=(start_date, end_date))
@@ -24,10 +25,9 @@ def index(request):
     cpu_data = CPUInfo.objects.filter(date__range=(start_date, end_date))
     # print(cpu_data)
 
-
     # print(disk_data)
     # for item in disk_data:
     #     print(item.used_percent,item.mount_point)
-    context = {"result": result, "memory_data": memory_data,"disk_data":disk_data,"cpu_data":cpu_data,}
+    context = {"result": result, "memory_data": memory_data, "disk_data": disk_data, "cpu_data": cpu_data, }
 
     return render(request, "index.html", context)
