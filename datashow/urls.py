325599043fe238ecb20cff8ebde0ee65e8c7d7d6
seancheng <seancheng33@gmail.com>
2021-05-07 15:34:57 +0800

    完成说有页面都需要登陆后才能查看

--- datashow/urls.py
+++ datashow/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('lastb.html', views.lastb, name='lastb'),
     path('config.html', views.config, name='config'),
     path('about.html', views.about, name='about'),
-    path('login.html', views.test, name='test'),
+    path('login.html', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
--- datashow/views.py
+++ datashow/views.py
@@ -1,14 +1,18 @@
 from datetime import datetime, timedelta
 
+from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from api.models import *
 import redis
 
 
 # Create your views here.
-# @login_required
+@login_required(login_url='login.html')     # 后面的login_url指明了登录页面。如果没有指明，就是默认的account/login的路径
 def index(request):
+    print(request.method)
     # 因为这里我是要做单机的，所以这个我只取最后的数据
     result = MechineInfo.objects.last()
 
@@ -38,7 +42,7 @@ def index(request):
 
     return render(request, "index.html", context)
 
-# @login_required
+@login_required(login_url='login.html')
 def cpu(request):
     # 获取当前的时间，用于获取数据时的时间范围用，设定的开始时间和结束时间为当前时间的24小时内
     now_time = datetime.now()
@@ -56,7 +60,7 @@ def cpu(request):
     context = {"cpu_data":cpu_data,"all_data":all_data[:640]}
     return render(request, "cpu.html", context)
 
-# @login_required
+@login_required(login_url='login.html')
 def memory(request):
 
     # 获取当前的时间，用于获取数据时的时间范围用，设定的开始时间和结束时间为当前时间的24小时内
@@ -73,7 +77,7 @@ def memory(request):
     context = {"mem_data": mem_data, "mem_all": mem_all}
     return render(request, "memory.html", context)
 
-# @login_required
+@login_required(login_url='login.html')
 def disk(request):
 
     # 获取挂载点信息，并去重，用于下面查询各个挂载点自己的信息，
@@ -88,6 +92,7 @@ def disk(request):
     context = {'disk_list': disk_list, 'disk_data': disk_data}
     return render(request, "disk.html", context)
 
+@login_required(login_url='login.html')
 def lastb(request):
     # 获取当前的总登陆失败的数据总量
     failed_count = LoginFailed.objects.count()
@@ -120,30 +125,45 @@ def lastb(request):
     return render(request, "lastb.html", context)
 
 
-# @login_required
+@login_required(login_url='login.html')
 def config(request):
 
     context = {}
     return render(request, "config.html", context)
 
-# @login_required
+@login_required(login_url='login.html')
 def about(request):
 
-    context = {'title': "关于本程序的title"}
+    context = {}
     return render(request, "about.html", context)
 
 
-def test(request):
+def login(request):
+    # 超管用户名是用python manage.py createsuperuser创建的（admin/admin123）
+    # print(request.method)
 
-    # 获取当前的时间，用于获取数据时的时间范围用，设定的开始时间和结束时间为当前时间的24小时内
-    now_time = datetime.now()
+    # 判断是不是post过来的数据，是的话，就是提交登陆数据的操作
+    if request.method == 'POST':
+        u = request.POST.get('username')
+        p = request.POST.get('password')
+        # 利用知道的认证功能，把页面post过来的用户名和密码，利用该功能验证并返回user对象，
+        # 这个user对象在下面的登陆中需要用到。
+        user = auth.authenticate(username=u,password=p)
+        print(u,p)
+        # 如果user对象有返回，就是用户是存在的，可以调用login函数登陆，如果没有，就是验证失败，返回错误提示信息。
+        if user is not None:
+            auth.login(request,user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            context = {"error": "用户名或密码错误，请核对后重试或联系系统管理员。"}
+            return render(request, "login.html", context)
 
-    start_date = now_time - timedelta(minutes=29, seconds=59)
-    end_date = now_time
-
-    # 这里获取的是内存的信息，使用的是时间范围。
-    mem_data = MemInfo.objects.filter(date__range=(start_date, end_date))
-    mem_all = MemInfo.objects.all().order_by('-date')
-
-    context = {"mem_data":mem_data,"mem_all":mem_all[:500]}
+    context = {"error":"请登陆系统已正常使用各种功能。"}
     return render(request, "login.html", context)
+
+def logout(request):
+    auth.logout(request)
+
+    print("logout")
+    context = {"error":"已经退出系统，请重新登陆。"}
+    return HttpResponseRedirect(reverse('login'), context)
